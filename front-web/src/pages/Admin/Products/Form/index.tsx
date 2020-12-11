9d70317b873e440cc580353ea0e42b5b8dac147b
Aurélio L. Andrade <aurelio@sistemico.com.br>
2020-12-11 15:06:22 -0300

    Add base admin form structure

---
+++ front-web/src/pages/Admin/Products/Form/index.tsx
@@ -0,0 +1,18 @@
+import BaseForm from 'pages/Admin/components/BaseForm';
+import React from 'react';
+import './styles.scss';
+
+const Form = () => {
+
+    return (
+        <BaseForm title="CADASTRAR UM PRODUTO">
+            <div className="row">
+                <div className="col-6">
+                    <input type="text" className="form-control" />
+                </div>
+            </div>
+        </BaseForm>
+    );
+};
+
+export default Form;
