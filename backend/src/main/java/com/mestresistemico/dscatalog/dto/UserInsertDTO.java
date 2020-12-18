a20afca38b31cd60f567992fcbf462f181727a15
Aurélio L. Andrade <aurelio@sistemico.com.br>
2020-12-18 18:02:30 -0300

    Custom constraint validator

--- backend/src/main/java/com/mestresistemico/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/mestresistemico/dscatalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.mestresistemico.dscatalog.dto;
 
+import com.mestresistemico.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO{
 	private static final long serialVersionUID = 1L;
 	
