5bd67502674e9adaacd14187db178bdc5a7f964c
Aurélio L. Andrade <aurelio@sistemico.com.br>
2021-02-15 15:35:16 -0300

    Add price input

--- front-web/src/pages/Admin/Products/Form/index.tsx
+++ front-web/src/pages/Admin/Products/Form/index.tsx
@@ -7,8 +7,9 @@ import { toast } from 'react-toastify';
 import { useHistory, useParams } from 'react-router-dom';
 import Select from 'react-select';
 import { Category } from 'core/types/Product';
+import PriceField from './PriceField';
 
-type FormState = {
+export type FormState = {
     name: string;
     price: string;
     imgUrl: string;
@@ -106,6 +107,7 @@ const Form = () => {
                                 classNamePrefix='categories-select'
                                 placeholder='Categorias'
                                 isLoading={isLoadingCategories}
+                                defaultValue=''
                                 isMulti
                             />
                             {errors.categories && (
@@ -115,11 +117,7 @@ const Form = () => {
                             )}
                         </div>
                         <div className='margin-bottom-30'>
-                            <input type="number"
-                                name="price"
-                                className="form-control input-base"
-                                ref={register({ required: "Campo obrigatório" })}
-                                placeholder={"Preço"} />
+                            <PriceField control={control}/>
                             {errors.price && (
                                 <div className='invalid-feedback d-block'>
                                     {errors.price.message}
